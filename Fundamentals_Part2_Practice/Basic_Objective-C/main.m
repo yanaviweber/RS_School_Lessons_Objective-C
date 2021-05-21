deb746bcb6516d26acf3d841fac14b364240d8fb
Vi Jane <creative.mashine@gmail.com>
2021-05-21 22:57:58 +0300

    feat: add import RSPerson.h in our main file. Add new functionality in the main file with RSPerson

--- Fundamentals_Part2_Practice/Basic_Objective-C/main.m
+++ Fundamentals_Part2_Practice/Basic_Objective-C/main.m
@@ -1,10 +1,12 @@
 #import <Foundation/Foundation.h>
+#import "RSPerson.h"
 
 int main(int argc, char **argv) {
 
     @autoreleasepool {
+        RSPerson *object = [[RSPerson alloc] initWithName:@"Mike"];
 
-
+        [object sayHello];
     }
 
     return 0;
