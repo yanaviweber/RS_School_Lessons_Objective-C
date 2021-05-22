4602905e19c966eb0bb9bc9a6cab06cc7de77f9a
Vi Jane <creative.mashine@gmail.com>
2021-05-23 00:35:25 +0300

    feat: add new project on the third part of our lesson Objective-C Fundamentals

---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
