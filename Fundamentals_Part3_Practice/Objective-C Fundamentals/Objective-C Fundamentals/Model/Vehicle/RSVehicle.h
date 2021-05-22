d57f14028eb13f244b63309b17d928cb2523b3b0
Vi Jane <creative.mashine@gmail.com>
2021-05-23 02:41:16 +0300

    feat: add new functionality in the class RSVehicle

--- Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Vehicle/RSVehicle.h
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Vehicle/RSVehicle.h
@@ -6,10 +6,17 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RSPassenger.h"
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RSVehicle : NSObject
+
+@property (nonatomic, strong, readonly) RSPassenger *captain;
+@property (nonatomic, assign, readonly) BOOL running;
+
+- (instancetype)initWithPassenger:(RSPassenger *)captain;
+- (void)toggleEngine:(BOOL)start;
 
 @end
 
--- Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Vehicle/RSVehicle.m
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Vehicle/RSVehicle.m
@@ -7,6 +7,28 @@
 
 #import "RSVehicle.h"
 
+@interface RSVehicle ()
+
+@property (nonatomic, strong, readwrite) RSPassenger *captain;
+@property (nonatomic, assign, readwrite) BOOL running;
+
+@end
+
 @implementation RSVehicle
+
+- (instancetype)initWithPassenger:(RSPassenger *)captain {
+    self = [super init];
+    if (self) {
+        _captain = captain;
+    }
+    return self;
+}
+
+
+- (void)toggleEngine:(BOOL)start {
+    
+    
+    
+}
 
 @end
