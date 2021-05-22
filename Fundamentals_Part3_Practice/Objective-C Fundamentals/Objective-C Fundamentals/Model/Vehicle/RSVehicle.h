39a3ac75ab44426ab7469ab7c64cdacb39538ed5
Vi Jane <creative.mashine@gmail.com>
2021-05-23 02:06:26 +0300

    feat: create new class RSVehicle and add new empty files to this class

---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Vehicle/RSVehicle.h
@@ -0,0 +1,16 @@
+//
+//  RSVehicle.h
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSVehicle : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Vehicle/RSVehicle.m
@@ -0,0 +1,12 @@
+//
+//  RSVehicle.m
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import "RSVehicle.h"
+
+@implementation RSVehicle
+
+@end
