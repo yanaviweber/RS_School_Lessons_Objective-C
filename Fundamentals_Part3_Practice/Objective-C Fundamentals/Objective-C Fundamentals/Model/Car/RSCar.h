213be4389846a3c1196085b440f6352c3cf7df8d
Vi Jane <creative.mashine@gmail.com>
2021-05-23 02:58:34 +0300

    feat: create a new class RSCar and add the new empty files to this class

---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Car/RSCar.h
@@ -0,0 +1,16 @@
+//
+//  RSCar.h
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import "RSVehicle.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSCar : RSVehicle
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Car/RSCar.m
@@ -0,0 +1,14 @@
+//
+//  RSCar.m
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import "RSCar.h"
+
+@implementation RSCar
+
+
+
+@end
