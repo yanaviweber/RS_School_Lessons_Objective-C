03094328297f27222b75cfe6e6fe3946a95c935b
Vi Jane <creative.mashine@gmail.com>
2021-05-23 00:44:40 +0300

    feat: create new class RSPassenger. Add new files of this class

---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Passenger/RSPassenger.h
@@ -0,0 +1,16 @@
+//
+//  RSPassenger.h
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSPassenger : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Passenger/RSPassenger.m
@@ -0,0 +1,12 @@
+//
+//  RSPassenger.m
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import "RSPassenger.h"
+
+@implementation RSPassenger
+
+@end
