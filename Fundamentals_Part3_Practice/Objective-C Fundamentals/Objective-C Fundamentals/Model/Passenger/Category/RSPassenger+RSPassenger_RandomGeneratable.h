02a9246e890562bccb55e1a00dbf3e6dc5a474f4
Vi Jane <creative.mashine@gmail.com>
2021-05-23 04:19:27 +0300

    feat: create new class and category RSPassenger + RandomGeneration. Add new files to this

---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Passenger/Category/RSPassenger+RSPassenger_RandomGeneratable.h
@@ -0,0 +1,16 @@
+//
+//  RSPassenger+RSPassenger_RandomGeneratable.h
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import "RSPassenger.h"
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface RSPassenger (RSPassenger_RandomGeneratable)
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Passenger/Category/RSPassenger+RSPassenger_RandomGeneratable.m
@@ -0,0 +1,12 @@
+//
+//  RSPassenger+RSPassenger_RandomGeneratable.m
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import "RSPassenger+RSPassenger_RandomGeneratable.h"
+
+@implementation RSPassenger (RSPassenger_RandomGeneratable)
+
+@end
