9628230abd986ef79465472a2e6e4797c72bc5b1
Vi Jane <creative.mashine@gmail.com>
2021-05-23 03:35:05 +0300

    feat: create protocol file RSEnterable.h for our class RSCarPort

---
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Port/CarPort/RSEnterable.h
@@ -0,0 +1,16 @@
+//
+//  RSEnterable.h
+//  Objective-C Fundamentals
+//
+//  Created by Viktoriia Jane Ro on 23.05.2021.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@protocol RSEnterable <NSObject>
+
+@end
+
+NS_ASSUME_NONNULL_END
