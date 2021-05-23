5ee686d284d4f25f5f7ab5a8a65ba321f3ec1f61
Vi Jane <creative.mashine@gmail.com>
2021-05-23 03:17:24 +0300

    feat: add new functionality in the new class RSCarPort

--- Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Port/CarPort/RSCarPort.h
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Port/CarPort/RSCarPort.h
@@ -6,10 +6,14 @@
 //
 
 #import <Foundation/Foundation.h>
+#import "RSCar.h"
+
 
 NS_ASSUME_NONNULL_BEGIN
 
 @interface RSCarPort : NSObject
+
+- (void)launchCar:(RSCar *)car;
 
 @end
 
--- Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Port/CarPort/RSCarPort.m
+++ Fundamentals_Part3_Practice/Objective-C Fundamentals/Objective-C Fundamentals/Model/Port/CarPort/RSCarPort.m
@@ -7,6 +7,27 @@
 
 #import "RSCarPort.h"
 
+@interface RSCarPort ()
+
+@property (nonatomic, strong) NSMutableArray<RSCar *> *trackPool;
+
+@end
+
 @implementation RSCarPort
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        _trackPool = [NSMutableArray new];
+    }
+    return self;
+}
+
+- (void)launchCar:(RSCar *)car {
+    [self.trackPool addObject:car];
+    [car toggleEngine:YES];
+    [car move];
+}
 
 @end
